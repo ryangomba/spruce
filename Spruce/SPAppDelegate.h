71c39ae68b236dfc99df40a29d091ab3d99dd02a
Ryan Gomba <ryan@ryangomba.com>
2012-09-01 08:30:47 -0700

    Initial Commit

---
+++ Spruce/SPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SPAppDelegate.h
+//  Spruce
+//
+//  Created by Ryan on 9/1/12.
+//  Copyright (c) 2012 Ryan Gomba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
